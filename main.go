c019d01448593329a2d259083f7ec2f089dad7fe
Luis Urrea <lfurrea@gmail.com>
2019-07-08 13:42:57 -0600

    Changed color index

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
     phrases = append(phrases, faker.Hacker().Phrases()...)
   }
   output := strings.Join(phrases[:], "; ")
-  r, g, b := 255, 215, 0 //gold color
+  r, g, b := 255, 216, 0 //gold color
 
   for j := 0; j < len(output); j++ {
       fmt.Printf("\033[38;2;%d;%d;%dm%c\033[0m", r, g, b, output[j])
